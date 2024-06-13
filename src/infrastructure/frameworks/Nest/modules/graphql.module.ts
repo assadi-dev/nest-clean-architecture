dca7f667524fdb18441f4148a217886ee7ff91a1
Assadi <62776480+assadi-dev@users.noreply.github.com>
2024-06-13 10:47:32 +0200

    deplacement du schema.gql generé par nest

--- src/infrastructure/frameworks/Nest/modules/graphql.module.ts
+++ src/infrastructure/frameworks/Nest/modules/graphql.module.ts
@@ -10,7 +10,7 @@ import { join } from 'path';
       driver: ApolloDriver,
       autoSchemaFile: join(
         process.cwd(),
-        'src/infrastructure/adapter/graphQL/schema.gql',
+        'src/infrastructure/frameworks/Nest/schema.gql',
       ),
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
